0760e2011d0ae8508d618d5de0aa497fc32e5d57
Ian <ian.diks@uni-saarland.de>
2025-05-31 23:52:27 +0000

    corrected louvain clustering approach, now louvain clustering in beta

--- src/moving/standard.rs
+++ src/moving/standard.rs
@@ -43,6 +43,24 @@ where
         self.neighboring_clusters.resize(size, 0);
     }
 
+    fn calculate_modularity_gain(
+        &self,
+        node_weight: T,
+        k_i_in: T,
+        cluster_tot: T,
+        total_edge_weight_2m: T,
+    ) -> T {
+        // ΔQ = [Σin + ki,in]/2m - [(Σtot + ki)/2m]² - [Σin/2m - (Σtot/2m)² - (ki/2m)²]
+        // Simplifying: ΔQ = ki,in/2m - ki*Σtot/2m² - ki²/2m²
+        // Further: ΔQ = (ki,in - ki*Σtot/2m - ki²/2m) / 2m
+        
+        let two_m = total_edge_weight_2m;
+        let term1 = k_i_in / two_m;
+        let term2 = (node_weight * cluster_tot * self.resolution) / (two_m * two_m);
+        
+        term1 - term2
+    }
+
     pub fn iterate<C, R>(
         &mut self,
         network: &Network<T, T>,
@@ -53,20 +71,26 @@ where
         C: NetworkGrouping,
         R: RngCore,
     {
-        let mut update = false;
         let node_count = network.nodes();
+        if node_count == 0 {
+            return false;
+        }
+
         self.ensure_capacity(node_count);
+        
+        // Initialize cluster weights and counts
+        self.cluster_weights[..node_count].fill(T::zero());
+        self.nodes_per_cluster[..node_count].fill(0);
+        self.edge_weight_per_cluster[..node_count].fill(T::zero());
 
-        self.cluster_weights.fill(T::zero());
-        self.nodes_per_cluster.fill(0);
-        self.edge_weight_per_cluster.fill(T::zero());
-
+        // Calculate initial cluster weights
         for i in 0..node_count {
             let cluster = clustering.get_group(i);
             self.cluster_weights[cluster] += network.weight(i);
             self.nodes_per_cluster[cluster] += 1;
         }
 
+        // Find unused clusters
         let mut num_unused_clusters = 0;
         for i in (0..node_count).rev() {
             if self.nodes_per_cluster[i] == 0 {
@@ -74,98 +98,99 @@ where
                 num_unused_clusters += 1;
             }
         }
-        //println!("Number of unused clusters: {:?}", num_unused_clusters);
 
+        // Shuffle node order once per iteration
         self.node_order.clear();
         self.node_order.extend(0..node_count);
         self.node_order.shuffle(rng);
 
-        let total_edge_weight =
-            T::from_f64(network.get_total_edge_weight().to_f64().unwrap()).unwrap();
-        let mut num_unstable_nodes = node_count;
-        let mut i = 0;
+        let total_edge_weight_2m = network.get_total_edge_weight() * T::from(2).unwrap();
+        let mut global_update = false;
 
-        //println!("Total edge weight: {:?}, num unstable nodes: {:?}, i = {:?}", total_edge_weight.to_f64().unwrap(), num_unstable_nodes, i);
+        // Keep iterating until no improvements are found
+        let mut local_improvement = true;
+        while local_improvement {
+            local_improvement = false;
 
-        while num_unstable_nodes > 0 {
-            //println!("ITERATION | Total edge weight: {:?}, num unstable nodes: {:?}, i = {:?}", total_edge_weight.to_f64().unwrap(), num_unstable_nodes, i);
-            let node = self.node_order[i];
-            let current_cluster = clustering.get_group(node);
-            //println!("ITERATION | Node: {:?}, Current Cluster: {:?}", node, current_cluster);
+            for &node in &self.node_order {
+                let current_cluster = clustering.get_group(node);
+                let node_weight = network.weight(node);
 
-            // Remove node from current cluster
-            let node_weight = T::from_f64(network.weight(node).to_f64().unwrap()).unwrap();
-            self.cluster_weights[current_cluster] -= node_weight;
-            self.nodes_per_cluster[current_cluster] -= 1;
+                // Remove node from current cluster
+                self.cluster_weights[current_cluster] -= node_weight;
+                self.nodes_per_cluster[current_cluster] -= 1;
 
-            if self.nodes_per_cluster[current_cluster] == 0 {
-                self.unused_clusters[num_unused_clusters] = current_cluster;
-                num_unused_clusters += 1;
-                //println!("ITERATION | Nodes per cluster == 0, num unused clusters {:?}", num_unused_clusters);
-            }
-
-            // Find neighboring clusters
-            self.neighboring_clusters[0] = self.unused_clusters[num_unused_clusters - 1];
-            let mut num_neighboring_clusters = 1;
-
-            for (target, weight) in network.neighbors(node) {
-                let neighbor_cluster = clustering.get_group(target);
-                let edge_weight = T::from_f64(weight.to_f64().unwrap()).unwrap();
-                //println!("ITERATION | FORLOOP | target: {:?}, neighbor cluster: {:?}, edge_weight: {:?}", target, neighbor_cluster, edge_weight.to_f64().unwrap());
-                if self.edge_weight_per_cluster[neighbor_cluster] == T::zero() {
-                    //println!("ITERATION | FORLOOP | Is T::zero, edge_weight_per_cluster");
-                    self.neighboring_clusters[num_neighboring_clusters] = neighbor_cluster;
-                    num_neighboring_clusters += 1;
+                if self.nodes_per_cluster[current_cluster] == 0 {
+                    self.unused_clusters[num_unused_clusters] = current_cluster;
+                    num_unused_clusters += 1;
                 }
-                self.edge_weight_per_cluster[neighbor_cluster] += edge_weight;
-            }
 
-            // Find best cluster
-            let mut best_cluster = current_cluster;
-            let mut max_quality_increment = self.edge_weight_per_cluster[current_cluster]
-                - (node_weight * self.cluster_weights[current_cluster] * self.resolution)
-                    / (T::from_f64(2.0).unwrap() * total_edge_weight);
+                // Clear edge weights from previous iteration
+                self.edge_weight_per_cluster[..node_count].fill(T::zero());
 
-            //println!("ITERATION | Best Cluster {:?} Max Quality Increment {:?}", best_cluster, max_quality_increment.to_f64().unwrap());
+                // Find neighboring clusters and calculate edge weights
+                self.neighboring_clusters[0] = if num_unused_clusters > 0 {
+                    self.unused_clusters[num_unused_clusters - 1]
+                } else {
+                    current_cluster // fallback
+                };
+                let mut num_neighboring_clusters = 1;
 
-            for &cluster in &self.neighboring_clusters[..num_neighboring_clusters] {
-                let quality_increment = self.edge_weight_per_cluster[cluster]
-                    - (node_weight * self.cluster_weights[cluster] * self.resolution)
-                        / num_traits::Float::powi(T::from_f64(2.0).unwrap() * total_edge_weight, 2);
-                //println!("ITERATION | Cluster {:?} Quality Increment {:?}", cluster, quality_increment.to_f64().unwrap());
-                if quality_increment > max_quality_increment
-                    || (quality_increment == max_quality_increment && cluster < best_cluster)
-                {
-                    best_cluster = cluster;
-                    max_quality_increment = quality_increment;
-                    //println!("ITERATION | Passes if for quality improvement")
+                for (target, weight) in network.neighbors(node) {
+                    let neighbor_cluster = clustering.get_group(target);
+                    
+                    if self.edge_weight_per_cluster[neighbor_cluster] == T::zero() && 
+                       neighbor_cluster != self.neighboring_clusters[0] {
+                        self.neighboring_clusters[num_neighboring_clusters] = neighbor_cluster;
+                        num_neighboring_clusters += 1;
+                    }
+                    self.edge_weight_per_cluster[neighbor_cluster] += weight;
                 }
-                self.edge_weight_per_cluster[cluster] = T::zero();
+
+                // Find best cluster
+                let mut best_cluster = current_cluster;
+                let mut max_quality_increment = self.calculate_modularity_gain(
+                    node_weight,
+                    self.edge_weight_per_cluster[current_cluster],
+                    self.cluster_weights[current_cluster],
+                    total_edge_weight_2m,
+                );
+
+                for &cluster in &self.neighboring_clusters[..num_neighboring_clusters] {
+                    let quality_increment = self.calculate_modularity_gain(
+                        node_weight,
+                        self.edge_weight_per_cluster[cluster],
+                        self.cluster_weights[cluster],
+                        total_edge_weight_2m,
+                    );
+
+                    if quality_increment > max_quality_increment ||
+                       (quality_increment == max_quality_increment && cluster < best_cluster) {
+                        best_cluster = cluster;
+                        max_quality_increment = quality_increment;
+                    }
+                }
+
+                // Update cluster assignment
+                self.cluster_weights[best_cluster] += node_weight;
+                self.nodes_per_cluster[best_cluster] += 1;
+
+                if best_cluster == self.unused_clusters[num_unused_clusters - 1] {
+                    num_unused_clusters -= 1;
+                }
+
+                if best_cluster != current_cluster {
+                    clustering.set_group(node, best_cluster);
+                    local_improvement = true;
+                    global_update = true;
+                }
             }
-
-            // Update cluster assignment
-            self.cluster_weights[best_cluster] += node_weight;
-            self.nodes_per_cluster[best_cluster] += 1;
-
-            if best_cluster == self.unused_clusters[num_unused_clusters - 1] {
-                num_unused_clusters -= 1;
-            }
-
-            num_unstable_nodes -= 1;
-
-            if best_cluster != current_cluster {
-                clustering.set_group(node, best_cluster);
-                update = true;
-            }
-
-            i = (i + 1) % node_count;
-            //println!("END: {:?} best cluster {:?} num unstable nodes {:?} num unused clusters {:?}", i, best_cluster, num_unstable_nodes, num_unused_clusters)
         }
 
-        if update {
+        if global_update {
             clustering.normalize_groups();
         }
 
-        update
+        global_update
     }
 }
